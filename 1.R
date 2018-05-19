a2b1808f27cfe2c67945a1a8ff739f6d290610e1
1ngR <gslinger@users.noreply.github.com>
2018-05-19 17:44:55 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,7 +1,3 @@
-# Project Euler Problem [1] # 
-#   by github/aev1tas       #
-#           in R            #
-
 i<-999
 fn <- function(x) {x<- x*as.integer(i/x)*(1+as.integer(i/x))/2}
 fn(3)+fn(5)-fn(15)
